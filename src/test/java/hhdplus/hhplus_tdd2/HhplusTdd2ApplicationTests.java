5de371c581972d5582f642ad5d76a5da44b55909
sehyeogi365 <lsh27572757@gmail.com>
2024-12-21 22:50:29 +0900

    첫커밋

---
+++ src/test/java/hhdplus/hhplus_tdd2/HhplusTdd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hhdplus.hhplus_tdd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusTdd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
